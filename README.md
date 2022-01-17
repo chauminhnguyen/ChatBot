40e552e9bc4b9eabbc523e837212e33158dc3fa0
minhchaunguyen <minhchauss123456@gmail.com>
2022-01-17 12:52:35 +0700

    add API

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ChatBot
 
 Chat Bot website written with Vue, BootStrap and Firebase (in progress).
+
+The Conv AI is used from HuggingFace and is built into API by Flask for js to call.
--- public/chatbot/main.py
+++ public/chatbot/main.py
@@ -4,17 +4,30 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json
 
+# Initialize FireStore
+cred_obj = credentials.Certificate('./my-project.json')
+default_app = firebase_admin.initialize_app(cred_obj)
+db = firestore.client()
+
+def get_data_from_firebase():
+    doc_ref = db.collection(u'user').document(u'history')
+    data = doc_ref.get()
+    return data.to_dict()
+
+def push_data_to_firebase(data):
+    doc_ref = db.collection(u'user').document(u'history')
+    doc_ref.update({
+        u'convos': firestore.ArrayUnion(data)
+    })
+
 
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
 
-def generate_conv(user_data):
-    with open('data.json') as f:
-        data = json.load(f)
-
+def generate_conv(user_data):    
+    data = get_data_from_firebase()
     bot_input_ids = None
-    history = data['history']
-    print('hist', history[-2:])
+    history = data['convos']
     for l in history[3:]:
         res = tokenizer.encode(l + tokenizer.eos_token, return_tensors='pt')
         bot_input_ids = torch.cat([bot_input_ids, res], dim=-1) if bot_input_ids != None else res
@@ -31,22 +44,7 @@ def generate_conv(user_data):
     # pretty print last ouput tokens from bot
     response = tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
+    new_data = [user_data, response]
+    push_data_to_firebase(new_data)
+
     return response
-
-# Initialize FireStore
-cred_obj = credentials.Certificate('./my-project.json')
-default_app = firebase_admin.initialize_app(cred_obj)
-db = firestore.client()
-
-def get_data_from_firebase():
-    doc_ref = db.collection(u'user').document(u'history')
-    data = doc_ref.get()
-    return data.to_dict()
-
-def push_data_to_firebase(data):
-    doc_ref = db.collection(u'user').document(u'history')
-    doc_ref.update({
-        u'bot': firestore.ArrayUnion([data])
-    })
-
-# get_data_from_firebase()
